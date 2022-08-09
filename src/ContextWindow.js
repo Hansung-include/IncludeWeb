3126322db67a0faeeebe1a7235405b323b4bc5b8
hjinwoo1028 <hjwoo1028@naver.com>
2022-08-09 23:02:17 +0900

    ContextWindow_허진우

--- src/ContextWindow.js
+++ src/ContextWindow.js
@@ -1,13 +1,69 @@
 import React from "react"
+import { Provider, useSelector, useDispatch } from 'react-redux';
+
 import "./ContextWindow.css"
+import reduer from "./DpwindowStore";
+import activity from "./activity"
 
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 
 const RouteBtn = (props) => {
+    const dispatch = useDispatch()
     return(
         <div>
-            <Button size="medium">{ props.name }</Button>
+            <div id="btn-style" onClick={(e) => {
+                dispatch({ type : "SET_SEMESTER", newSemester : props.name })
+            }}>
+                <Button >{ props.name }</Button>
+            </div>
+        </div>
+    )
+}
+
+const ActivityLog = (props) => {
+    // console.log(props.log.activityName)
+    // console.log(props.log.participant)
+    // console.log(props.log.contents)
+
+    const getActivityName = log => {
+        if (props.log.activityName === undefined) return ""
+        else return props.log.activityName
+    }
+
+    const getParticipant = log => {
+        if (props.log.participant === undefined) return ""
+        else return props.log.participant.map(m => <span>{m + " "}</span>)
+    }
+
+    const getContents = log => {
+        if (props.log.contents === undefined) return ""
+        else return props.log.contents
+    }
+
+    return (
+        <div>
+            <div className="topWhiteSpace" />
+            <h4>활동명 : { getActivityName(props.log) }
+            </h4>
+            <h5>참여 동아리원 : { getParticipant(props.log) } 
+            </h5>
+            <h5>활동 설명 : { getContents(props.log) }</h5>
+        </div>
+    )
+} 
+
+const DisplayWindow = () => {
+    const inform = useSelector(state => { return state })
+    
+    return (
+        <div id="DpWindow">
+            <h3>학기 : { inform.semester }</h3>
+            {
+                activity[inform.semester].map(a => <ActivityLog log={a} />)
+            }
+            <hr />
+            <h4>{inform.semester} 명단</h4>
         </div>
     )
 }
@@ -30,15 +86,15 @@ const ContextWindow = () => {
     }
     return(
         <div>
-            <div>
-                <div className="topWhiteSpace" />
-                <div id="mainWindow">
-                    <ButtonGroup variant="text" orientation="vertical" aria-label="outlined primary button group" size="medium">
-                        { semester.map(s => <RouteBtn name={s}/>) }
-                    </ButtonGroup>
-                    <div id="viewer">.</div>
-                </div>
+        <Provider store={reduer}>
+            <div className="topWhiteSpace" />
+            <div id="mainWindow">
+                <ButtonGroup variant="text" orientation="vertical" aria-label="outlined primary button group" size="medium">
+                    { semester.map(s => <RouteBtn name={s}/>) }
+                </ButtonGroup>
+                <DisplayWindow />
             </div>
+        </Provider>
         </div>
     );
 }
