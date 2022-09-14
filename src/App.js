1faec0dc4fce0b05d98e84becf1d062f0e568be0
mj0107 <alswns1285@gmail.com>
2022-09-14 16:25:02 +0900

    Feat: TopBar 라우팅
    - #include 로고 클릭시 기본 페이지로 이동('/' 루트 페이지)
    - Main 클릭시 ~/Main으로 이동
    - Contents 클릭시
    ~/Contents로 이동

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import TopBar from './TopBar';
+import TopBar from "./TopBar";
 import BottomBar from "./BottomBar";
-import ContextWindow from './ContextWindow';
+import Main from "./main/Home";
 import "./App.css";
-// import Test from "./Menus/TestMenu1";
-import logo from './logo.svg';
-import Home from "./main/Home";
+import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
-      <ContextWindow />
-      {/* <Test /> */}
       <BottomBar />
-      <Home/>
     </div>
   );
 }
